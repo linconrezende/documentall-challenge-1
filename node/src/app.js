5ec7651fd1e5d9680c97f308bccdca64805ba6f0
Lincon <lincon.gomes.cfpt@gmail.com>
2021-05-28 21:15:34 -0300

    Added a separated file to manage the database connection. The app, will start the express server. For now I just added some error handlers, 404 page and added some custom headers.

---
+++ node/src/app.js
@@ -0,0 +1,54 @@
+const express = require('express')
+const path = require('path')
+const logger = require('morgan')
+const cookieParser = require('cookie-parser')
+const Render = require('./controllers/resultController')
+// DB Connection
+require('./database/connection')
+
+// Definição dos routes
+// const fileRoutes = require('./routes/file') // uncomment when the file is created
+
+const app = express()
+
+// Using morgan to log
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Middleware before routes to append headers and other config
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    // Return informations about our methods
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, PATCH, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
+
+// Routes middleware to handle requests
+// app.use('/file', fileRoutes) // I will uncomment when I create the file
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  var err = new Error('Route not found')
+  err.status = 404
+  next(err)
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  Render.Res(res, err.status || 500, null, err && err.data ? err.data : null, err)
+})
+
+module.exports = app
