da2d0a8a2cfc8bc21d38e6b00b208ef549da29d1
Lincon <lincon.gomes.cfpt@gmail.com>
2021-05-28 21:30:15 -0300

    Added server.js

---
+++ node/server.js
@@ -0,0 +1,9 @@
+'use strict'
+const http = require('http')
+const app = require('./src/app')
+
+const port = 3000
+const server = http.createServer(app)
+
+server.listen(port)
+console.log(`Running on port ${port}`)
