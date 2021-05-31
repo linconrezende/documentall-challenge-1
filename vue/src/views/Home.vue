849d2af4199f0f112fc206b3cc006281cd7243e0
Lincon <lincon.gomes.cfpt@gmail.com>
2021-05-30 23:08:22 -0300

    Created new component to upload the file and update the API script to use custom headers

--- vue/src/views/Home.vue
+++ vue/src/views/Home.vue
@@ -1,15 +1,40 @@
 <template>
-  <hello-world />
+   <v-container fill-height fluid>
+      <v-row>
+        <v-col cols="6" class="mx-auto" style="border: solid black 1px;">
+          <component v-if="actualStep && actualStep.component" :is="actualStep.component.name" v-bind="actualStep.component.props" />
+        </v-col>
+      </v-row>
+    </v-container>
 </template>
 
 <script>
 import HelloWorld from "../components/HelloWorld";
+import UploadPhoneMessages from "../components/UploadPhoneMessages";
 
 export default {
   name: "Home",
-
+  data() {
+    return {
+      steps: {
+        1: {
+          component: {
+            name: 'HelloWorld',
+            props: null
+          }
+        }
+      },
+      actualStep: {
+        component: {
+          name: 'UploadPhoneMessages',
+          props: null
+        }
+      }
+    }
+  },
   components: {
     HelloWorld,
+    UploadPhoneMessages
   },
   created() {
     // var vm = this
